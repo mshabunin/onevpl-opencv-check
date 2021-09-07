a2eed222480ab4862315932eed4f556e52e2021b
Maksim Shabunin <maksim.shabunin@gmail.com>
2021-09-07 15:07:51 +0300

    Don't install opencv, run from build

--- build-opencv.sh
+++ build-opencv.sh
@@ -13,6 +13,6 @@ TAG_OPENCV=master
 [ ! -d opencv ] && git clone --depth=1 https://github.com/opencv/opencv --branch ${TAG_OPENCV}
 mkdir -p opencv/build
 pushd opencv/build
-cmake -GNinja -DCMAKE_INSTALL_PREFIX=${DST} -DINSTALL_TESTS=ON -DWITH_MFX=ON ..
-ninja install
+cmake -GNinja -DWITH_MFX=ON ..
+ninja
 popd
